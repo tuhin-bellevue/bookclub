48f65c7ae057ed1456edd6ae97c039f7c1f5363e
Tuhin <tudaw@my365.bellevue.edu>
2025-03-15 00:07:24 -0400

    Initial commit by Tuhin

---
+++ tuhin-springboot-app/src/main/java/com/bookclub/TuhinSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.bookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuhinSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuhinSpringbootAppApplication.class, args);
+	}
+
+}
