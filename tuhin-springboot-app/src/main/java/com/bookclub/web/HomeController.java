7dd40ffb95407405cdad4d85071d9e4420820393
Tuhin <tudaw@my365.bellevue.edu>
2025-03-22 23:01:52 -0400

    tuhin - week 2.2 project work

--- tuhin-springboot-app/src/main/java/com/bookclub/web/HomeController.java
+++ tuhin-springboot-app/src/main/java/com/bookclub/web/HomeController.java
@@ -1,4 +1,31 @@
 package com.bookclub.web;
 
-public class HpmeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String showHome(Model model){
+        return "index";
+    }
+
+    //@RequestMapping(method = RequestMethod.GET, path = "/about")
+    @GetMapping("/about")
+    public String showAboutUs(Model model){
+        return "about";
+    }
+
+
+    //@RequestMapping(method = RequestMethod.GET, path = "/contact")
+    @GetMapping("/contact")
+    public String showContactUs(Model model){
+        return "contact";
+    }
 }
