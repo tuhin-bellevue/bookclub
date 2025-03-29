36fadc0f21fd35b196207633123ef39800c1231a
Tuhin <tudaw@my365.bellevue.edu>
2025-03-29 01:16:46 -0400

    tuhin - week 3.2 project work

---
+++ tuhin-springboot-app/src/main/java/com/bookclub/service/impl/MemBookDao.java
@@ -0,0 +1,42 @@
+package com.bookclub.service.impl;
+
+import com.bookclub.model.Book;
+import com.bookclub.service.dao.BookDao;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MemBookDao implements BookDao {
+
+    private List<Book> books = new ArrayList<>();
+
+    public MemBookDao() {
+        Book book = new Book("isbn1", "Math","Math Book", 50, Arrays.asList("John D","Mark C","Jack D"));
+        Book book1 = new Book("isbn2", "Science","Science Book", 70, Arrays.asList("John D","Mark C","Jack D"));
+        Book book2 = new Book("isbn3", "Statistics","Stat Book", 30, Arrays.asList("John D","Mark C","Jack D"));
+        Book book3 = new Book("isbn4", "Story","Story Book", 100, Arrays.asList("John D","Mark C","Jack D"));
+        Book book4 = new Book("isbn5", "History","History Book", 600, Arrays.asList("John D","Mark C","Jack D"));
+
+        books.add(book);
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+    }
+
+    @Override
+    public List<Book> list() {
+        return this.books;
+    }
+
+    @Override
+    public Book find(String key) {
+        for(Book book: this.books){
+            if(book.getIsbn().equals(key)){
+                return book;
+            }
+        }
+        return new Book();
+    }
+}
--- tuhin-springboot-app/src/main/java/com/bookclub/web/HomeController.java
+++ tuhin-springboot-app/src/main/java/com/bookclub/web/HomeController.java
@@ -1,11 +1,12 @@
 package com.bookclub.web;
 
+import com.bookclub.model.Book;
+import com.bookclub.service.impl.MemBookDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -13,7 +14,27 @@ public class HomeController {
 
     @GetMapping
     public String showHome(Model model){
+        MemBookDao memBookDao = new MemBookDao();
+        List<Book> books = memBookDao.list();
+        model.addAttribute("books", books);
+        for (Book book: books){
+            System.out.println(book.toString());
+        }
         return "index";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    public String getMonthlyBook(@PathVariable("id") String id, Model model) {
+        String isbn = id;
+        System.out.println(id);
+
+        MemBookDao bookDao = new MemBookDao();
+        Book book = bookDao.find(isbn);
+
+        System.out.println(book.toString());
+
+        model.addAttribute("book", book);
+        return "monthly-books/view";
     }
 
     //@RequestMapping(method = RequestMethod.GET, path = "/about")
